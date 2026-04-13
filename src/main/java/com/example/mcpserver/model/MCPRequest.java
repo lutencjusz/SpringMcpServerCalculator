12fb358053c1a5fc7ad33aee50dfe9728f1f3f9e
Lutencjusz <lutencjusz@example.com>
2026-04-13 22:16:44 +0200

    Initial commit

--- src/main/java/com/example/mcpserver/model/MCPRequest.java
+++ src/main/java/com/example/mcpserver/model/MCPRequest.java
@@ -8,36 +8,4 @@ public class MCPRequest {
     private String method;
     private Object params;
     private String id;
-
-    public String getJsonrpc() {
-        return jsonrpc;
-    }
-
-    public void setJsonrpc(String jsonrpc) {
-        this.jsonrpc = jsonrpc;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    public Object getParams() {
-        return params;
-    }
-
-    public void setParams(Object params) {
-        this.params = params;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
